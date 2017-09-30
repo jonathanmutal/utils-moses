2ca7f90f91c13e280d596573d437c17cba262124
jonathanmutal <jonathanmutal95@gmail.com>
2017-09-29 23:03:49 -0300

    adding a new interface

---
+++ join_data.py
@@ -0,0 +1,19 @@
+import argparse
+from handle_data import handle_files
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Get a set of bytecode files \
+                                     and get a new one (with format UTF-16).')
+    parser.add_argument('-r', '--root', help='root directory', nargs=1,
+                        required=True)
+    parser.add_argument('-t', '--typet', help='training or tuning', nargs=1)
+    parser.add_argument('-f','--files',
+                        help='list of files. Example: file1.txt file2.txt',
+                        nargs='*', required=True)
+    parser.add_argument('-sl', '--src_language', help='language src',
+                        nargs=1, type=str, required=True)
+    parser.add_argument('-tl', '--trg_language', help='language trg',
+                        nargs=1, type=str, required=True)
+    args = parser.parse_args()
+    hf = handle_files(args.root[0], args.files, args.src_language[0], args.trg_language[0], args.typet[0])
+    hf.join_files() # group all files in one.
